3effdded61279adeead31c3d84546af498829286
rosa <natsukiarr@gmail.com>
2024-11-27 17:46:04 -0700

    CHANGE amqp to amqps

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@
 import { Semaforo } from './model/Semaforo';
 import { emitirEstado } from './services/semaforoservice'
 import { consumirEstado } from './services/semaforoservice';
+const fs = require('fs');
 const amqp = require('amqplib/callback_api');
 
 
 let isFirstTime: boolean = true;
 
-amqp.connect('amqp://localhost:5672', function (error0, connection) {
+const conf = {
+    cert: fs.readFileSync('./llaves/client_natsu_certificate.pem'),
+    key: fs.readFileSync('./llaves/client_natsu_key.pem'),
+    passphrase: 'hola123',
+    ca: [fs.readFileSync('./llaves/ca_certificate.pem')]
+}
+
+amqp.connect('amqps://localhost', conf, function (error0, connection) {
 
     if (error0) {
         throw error0;
